9e6e4d90dbfd281364847a416a54a5e7b204636f
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2025-07-28 23:48:59 -0400

    Fixed Tile sheet, and collisions

--- Scripts/musashi.gd
+++ Scripts/musashi.gd
@@ -1,8 +1,30 @@
 extends CharacterBody2D
 
-@export var speed : float = 50
+@export var speed : float = 200
+@export var animation_tree : AnimationTree
+
+var input : Vector2
+var playback : AnimationNodeStateMachinePlayback
+
+func _ready():
+	playback = animation_tree["parameters/playback"]
 
 func _physics_process(delta: float) -> void:
-	var input_vector = Input.get_vector("left", "right", "up", "down")
-	velocity = input_vector * speed
+	input = Input.get_vector("left", "right", "up", "down")
+	velocity = input * speed
 	move_and_slide()
+	select_animation()
+	update_animation_parameters()
+
+func select_animation():
+	if velocity == Vector2.ZERO:
+		playback.travel("Idle")
+	else:
+		playback.travel("Walk")
+
+func update_animation_parameters():
+	if input == Vector2.ZERO:
+		return
+	
+	animation_tree["parameters/Idle/blend_position"] = input
+	animation_tree["parameters/Walk/blend_position"] = input
