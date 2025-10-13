4e6356030bc309748a454ca1713aa5f184bef852
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2025-10-12 22:27:27 -0400

    Added Enemy
    Added Enemy State Machine and some behaviors. Need to flesh out both statemachines of character and enemy. Add enemy knock back and other behoviors (running, hitting)

---
+++ Scripts/swordsman.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var state_machine : CharacterStateMachine = $CharacterStateMachine
+
+@export var hit_state : State
+@export var starting_move_direction : Vector2 = Vector2.LEFT
+@export var speed : float = 170.0
+
+func _ready():
+	animation_tree.active = true
+
+func _physics_process(delta):
+	
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction : Vector2 = starting_move_direction
+	if direction && state_machine.check_if_can_move():
+		velocity.x = direction.x * speed
+	elif state_machine.current_state != hit_state:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
