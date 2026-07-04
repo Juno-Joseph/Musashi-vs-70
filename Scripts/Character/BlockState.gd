105a56ffc1e55a9da16408586943a3b5a92be541
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2026-07-04 19:48:10 -0400

    Fixed Block state :D
    Made it so that i can move after blocking

--- Scripts/Character/BlockState.gd
+++ Scripts/Character/BlockState.gd
@@ -5,19 +5,35 @@ class_name BlockState
 @export var attack_state : State
 @export var attack_node : String = "Attack"
 
+var timer : Timer
+
 func on_enter():
 	playback.travel("Block")
 	character.velocity = Vector2.ZERO
 	can_move = false
+	
+	if timer == null:
+		timer = find_child("Timer")
+	
+	if timer != null:
+		timer.start()
+	else:
+		print("ERROR: Timer node not found on", name)
 
 func on_exit():
-	character.can_move = true
+	can_move = true
 
 func _on_parry_area_hit(area):
 	if area.is_in_group("EnemyHitbox"):
+		if timer and not timer.is_stopped():
+			timer.stop()
+		
 		await character.freeze_frame(0.05, 0, .1)
 		playback.travel("Parry")
 		await get_tree().create_timer(0.3, true, false, true).timeout
 		# After parry finishes, we return to move_state or stay in block?
 		 # Usually, you return to move_state to give the player control back.
-		next_state = move_state
+		emit_signal("interrupt_state", move_state)
+
+func _on_timer_timeout():
+	emit_signal("interrupt_state", move_state)
